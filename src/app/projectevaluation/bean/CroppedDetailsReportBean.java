cdee2cf15089f142bf7ffc0836f08aff60b73ee8
hp <hp@KB-CGO-614-Yogesh.mshome.net>
2025-04-21 12:30:24 +0530

    commit by Yogesh

---
+++ src/app/projectevaluation/bean/CroppedDetailsReportBean.java
@@ -0,0 +1,107 @@
+package app.projectevaluation.bean;
+
+import java.math.BigDecimal;
+
+public class CroppedDetailsReportBean {
+	
+	private Integer stcode;	
+	private String stname;	
+	private Integer totproj;	
+	private BigDecimal cropcereals;	
+	private BigDecimal croppulses;	
+	private BigDecimal cropoilseed;	
+	private BigDecimal cropmillets;	
+	private BigDecimal cropother;	
+	private BigDecimal rice;	
+	private BigDecimal wheat;	
+	private BigDecimal pulses;	
+	private BigDecimal oilseed;	
+	private BigDecimal millets;	
+	private BigDecimal other;
+	
+	public Integer getStcode() {
+		return stcode;
+	}
+	public void setStcode(Integer stcode) {
+		this.stcode = stcode;
+	}
+	public String getStname() {
+		return stname;
+	}
+	public void setStname(String stname) {
+		this.stname = stname;
+	}
+	public Integer getTotproj() {
+		return totproj;
+	}
+	public void setTotproj(Integer totproj) {
+		this.totproj = totproj;
+	}
+	public BigDecimal getCropcereals() {
+		return cropcereals;
+	}
+	public void setCropcereals(BigDecimal cropcereals) {
+		this.cropcereals = cropcereals;
+	}
+	public BigDecimal getCroppulses() {
+		return croppulses;
+	}
+	public void setCroppulses(BigDecimal croppulses) {
+		this.croppulses = croppulses;
+	}
+	public BigDecimal getCropoilseed() {
+		return cropoilseed;
+	}
+	public void setCropoilseed(BigDecimal cropoilseed) {
+		this.cropoilseed = cropoilseed;
+	}
+	public BigDecimal getCropmillets() {
+		return cropmillets;
+	}
+	public void setCropmillets(BigDecimal cropmillets) {
+		this.cropmillets = cropmillets;
+	}
+	public BigDecimal getCropother() {
+		return cropother;
+	}
+	public void setCropother(BigDecimal cropother) {
+		this.cropother = cropother;
+	}
+	public BigDecimal getRice() {
+		return rice;
+	}
+	public void setRice(BigDecimal rice) {
+		this.rice = rice;
+	}
+	public BigDecimal getWheat() {
+		return wheat;
+	}
+	public void setWheat(BigDecimal wheat) {
+		this.wheat = wheat;
+	}
+	public BigDecimal getPulses() {
+		return pulses;
+	}
+	public void setPulses(BigDecimal pulses) {
+		this.pulses = pulses;
+	}
+	public BigDecimal getOilseed() {
+		return oilseed;
+	}
+	public void setOilseed(BigDecimal oilseed) {
+		this.oilseed = oilseed;
+	}
+	public BigDecimal getMillets() {
+		return millets;
+	}
+	public void setMillets(BigDecimal millets) {
+		this.millets = millets;
+	}
+	public BigDecimal getOther() {
+		return other;
+	}
+	public void setOther(BigDecimal other) {
+		this.other = other;
+	}
+
+}
