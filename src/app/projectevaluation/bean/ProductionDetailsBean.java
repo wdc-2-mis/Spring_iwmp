5124da1065f6ea722a453e84c2a19666631f0fab
hp <hp@KB-CGO-614-Yogesh.mshome.net>
2025-04-30 12:01:59 +0530

    bean commited by yogesh

---
+++ src/app/projectevaluation/bean/ProductionDetailsBean.java
@@ -0,0 +1,181 @@
+package app.projectevaluation.bean;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+public class ProductionDetailsBean {
+	
+	private Integer stcode;	
+	private String stname;	
+	private Integer totproj;	
+	private BigDecimal prefpoturnover;
+	private BigDecimal midfpoturnover;
+	private BigDecimal controlfpoturnover;
+	private BigDecimal prefpoincome;
+	private BigDecimal midfpoincome;
+	private BigDecimal controlfpoincome;
+	private BigDecimal preannualincomeshg;
+	private BigDecimal midannualincomeshg;
+	private BigDecimal controlannualincomeshg;
+
+	private BigInteger communitybasedshg; 
+	private BigInteger controlcommunitybasedshg; 
+	private BigInteger communitybasedfpo; 
+	private BigInteger controlcommunitybasedfpo; 
+	private BigInteger communitybasedug; 
+	private BigInteger controlcommunitybasedug; 
+	private BigInteger memberbasedshg; 
+	private BigInteger controlmemberbasedshg; 
+	private BigInteger memberbasedfpo; 
+	private BigInteger controlmemberbasedfpo; 
+	private BigInteger memberbasedug; 
+	private BigInteger controlmemberbasedug;
+	
+	public Integer getStcode() {
+		return stcode;
+	}
+	public void setStcode(Integer stcode) {
+		this.stcode = stcode;
+	}
+	public String getStname() {
+		return stname;
+	}
+	public void setStname(String stname) {
+		this.stname = stname;
+	}
+	public Integer getTotproj() {
+		return totproj;
+	}
+	public void setTotproj(Integer totproj) {
+		this.totproj = totproj;
+	}
+	public BigDecimal getPrefpoturnover() {
+		return prefpoturnover;
+	}
+	public void setPrefpoturnover(BigDecimal prefpoturnover) {
+		this.prefpoturnover = prefpoturnover;
+	}
+	public BigDecimal getMidfpoturnover() {
+		return midfpoturnover;
+	}
+	public void setMidfpoturnover(BigDecimal midfpoturnover) {
+		this.midfpoturnover = midfpoturnover;
+	}
+	public BigDecimal getControlfpoturnover() {
+		return controlfpoturnover;
+	}
+	public void setControlfpoturnover(BigDecimal controlfpoturnover) {
+		this.controlfpoturnover = controlfpoturnover;
+	}
+	public BigDecimal getPrefpoincome() {
+		return prefpoincome;
+	}
+	public void setPrefpoincome(BigDecimal prefpoincome) {
+		this.prefpoincome = prefpoincome;
+	}
+	public BigDecimal getMidfpoincome() {
+		return midfpoincome;
+	}
+	public void setMidfpoincome(BigDecimal midfpoincome) {
+		this.midfpoincome = midfpoincome;
+	}
+	public BigDecimal getControlfpoincome() {
+		return controlfpoincome;
+	}
+	public void setControlfpoincome(BigDecimal controlfpoincome) {
+		this.controlfpoincome = controlfpoincome;
+	}
+	public BigDecimal getPreannualincomeshg() {
+		return preannualincomeshg;
+	}
+	public void setPreannualincomeshg(BigDecimal preannualincomeshg) {
+		this.preannualincomeshg = preannualincomeshg;
+	}
+	public BigDecimal getMidannualincomeshg() {
+		return midannualincomeshg;
+	}
+	public void setMidannualincomeshg(BigDecimal midannualincomeshg) {
+		this.midannualincomeshg = midannualincomeshg;
+	}
+	public BigDecimal getControlannualincomeshg() {
+		return controlannualincomeshg;
+	}
+	public void setControlannualincomeshg(BigDecimal controlannualincomeshg) {
+		this.controlannualincomeshg = controlannualincomeshg;
+	}
+	public BigInteger getCommunitybasedshg() {
+		return communitybasedshg;
+	}
+	public void setCommunitybasedshg(BigInteger communitybasedshg) {
+		this.communitybasedshg = communitybasedshg;
+	}
+	public BigInteger getControlcommunitybasedshg() {
+		return controlcommunitybasedshg;
+	}
+	public void setControlcommunitybasedshg(BigInteger controlcommunitybasedshg) {
+		this.controlcommunitybasedshg = controlcommunitybasedshg;
+	}
+	public BigInteger getCommunitybasedfpo() {
+		return communitybasedfpo;
+	}
+	public void setCommunitybasedfpo(BigInteger communitybasedfpo) {
+		this.communitybasedfpo = communitybasedfpo;
+	}
+	public BigInteger getControlcommunitybasedfpo() {
+		return controlcommunitybasedfpo;
+	}
+	public void setControlcommunitybasedfpo(BigInteger controlcommunitybasedfpo) {
+		this.controlcommunitybasedfpo = controlcommunitybasedfpo;
+	}
+	public BigInteger getCommunitybasedug() {
+		return communitybasedug;
+	}
+	public void setCommunitybasedug(BigInteger communitybasedug) {
+		this.communitybasedug = communitybasedug;
+	}
+	public BigInteger getControlcommunitybasedug() {
+		return controlcommunitybasedug;
+	}
+	public void setControlcommunitybasedug(BigInteger controlcommunitybasedug) {
+		this.controlcommunitybasedug = controlcommunitybasedug;
+	}
+	public BigInteger getMemberbasedshg() {
+		return memberbasedshg;
+	}
+	public void setMemberbasedshg(BigInteger memberbasedshg) {
+		this.memberbasedshg = memberbasedshg;
+	}
+	public BigInteger getControlmemberbasedshg() {
+		return controlmemberbasedshg;
+	}
+	public void setControlmemberbasedshg(BigInteger controlmemberbasedshg) {
+		this.controlmemberbasedshg = controlmemberbasedshg;
+	}
+	public BigInteger getMemberbasedfpo() {
+		return memberbasedfpo;
+	}
+	public void setMemberbasedfpo(BigInteger memberbasedfpo) {
+		this.memberbasedfpo = memberbasedfpo;
+	}
+	public BigInteger getControlmemberbasedfpo() {
+		return controlmemberbasedfpo;
+	}
+	public void setControlmemberbasedfpo(BigInteger controlmemberbasedfpo) {
+		this.controlmemberbasedfpo = controlmemberbasedfpo;
+	}
+	public BigInteger getMemberbasedug() {
+		return memberbasedug;
+	}
+	public void setMemberbasedug(BigInteger memberbasedug) {
+		this.memberbasedug = memberbasedug;
+	}
+	public BigInteger getControlmemberbasedug() {
+		return controlmemberbasedug;
+	}
+	public void setControlmemberbasedug(BigInteger controlmemberbasedug) {
+		this.controlmemberbasedug = controlmemberbasedug;
+	}
+	
+	
+
+}
