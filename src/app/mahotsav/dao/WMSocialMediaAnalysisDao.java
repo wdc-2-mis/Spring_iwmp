c052c4e114ae7b8b426b5307efe3364c8959caad
nic <nic@host.docker.internal>
2026-01-27 09:31:31 +0530

    social media analysis report committed by Unnati

---
+++ src/app/mahotsav/dao/WMSocialMediaAnalysisDao.java
@@ -0,0 +1,16 @@
+package app.mahotsav.dao;
+
+import java.util.List;
+
+import app.mahotsav.bean.SocialMediaReport;
+import app.mahotsav.model.WatershedMahotsavMediaMaster;
+import app.model.IwmpDistrict;
+
+public interface WMSocialMediaAnalysisDao {
+	
+	List<IwmpDistrict> getDistrictList(int stateCode);
+	List<WatershedMahotsavMediaMaster> getPlatformList();
+	List<SocialMediaReport> getWMSocialMediaAnalysisReport(Integer stcd, Integer dcode, Integer media);
+//	List<SocialMediaReport> getWMSocialMediaAnalysisReport(Integer stcd, Integer dcode, Integer media);
+//	List<SocialMediaReport> getWMSocialMediaAnalysisReport(Integer stcd, Integer dcode, Integer media, String orderBy);
+}
